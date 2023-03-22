28ec980bc3674408a53f152312cad02e4ea9e800
Krzysztof Kruk <krzysztof.kruk@krzysztofs-macbook-pro.home>
2023-03-22 23:30:14 +0100

    todo list created

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
 func main() {
+	//todo write a docker-compose file with db
+	//todo find in memory db that can be used to work in tests
+	//todo write a repository & unit tests
+	//todo write a service layer & unit tests
+	//todo write handlers & unit tests
+	//todo write integration tests
+	//todo dockerize everything
 
 }
